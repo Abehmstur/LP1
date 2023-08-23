0002ddb42f8a39a4ddca4bdafe6e8ca4a420a460
abehmstur <matheus.medeiros970@gmail.com>
2023-08-22 21:34:29 -0300

    Aula 01 LP1

---
+++ Aula-01/aula_01.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x , y;
+    cout << "Digite um valor para x e para y em sequencia: ";
+    cin >> x >> y; 
+    cout << "Você digitou x:" << x << "Você digitou y:" << y << endl;;
+    return 0;
+}
