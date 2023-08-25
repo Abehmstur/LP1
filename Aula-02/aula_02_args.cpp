e74dd10d78a783070efcfb592204cdf35adf37ef
abehmstur <matheus.medeiros970@gmail.com>
2023-08-24 21:43:51 -0300

    Aula 2 continuacao

---
+++ Aula-02/aula_02_args.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+//argc é a qtd de elementos -1
+/* int main(int argc, char *argv[]){
+
+    cout << argv[0] << endl;
+    cout << argv[1] << endl;
+    cout << argv[2] << endl;
+    cout << argc << endl;
+
+    return 0;
+} */
+
+//cin considera espaços e quebras de linhas como fim
+//get line impede que isso aconteça
+/* int main(int argc, char *argv[]){
+
+    string nome, nome1;
+
+    cin >> nome1;
+
+    getline(cin, nome);
+
+    cout << nome << endl;
+
+    return 0;
+} */
+
+int main(int argc, char *argv[]){
+
+    int vetor[] = {0,1,2,3,4,5};
+
+    return 0;
+}
+
+/* strings
+string a = "jubilas";
+a.push_back("pro fim da string")
+a.insert(posicao, "item")
+a.size()
+a.substr(2"a partir do segundo indice",4"qtd de elementos que vai pegar")
+ */
---
+++ Aula-02/aula_02_for.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main (void){
+    int i = 0;
+    for( ; i < 10 ; i++){
+        cout << "Valor de i: " << i << endl;
+    }
+    
+    return 0;
+}
