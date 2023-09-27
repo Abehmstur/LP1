05b40fafce673793acc87394ebcac9f28c2202a3
abehmstur <matheus.medeiros970@gmail.com>
2023-09-26 21:43:00 -0300

    asdasdasdsadasd

---
+++ Aula19-09/aluno.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <map>
+#include "aluno.hpp"
+#include <fstream>
+
+using namespace std;
+
+map<string, notas> aluno;
+
+void cadastarAlunos(string nome, float n1, float n2, float n3){
+    aluno[nome].nota01 = n1;
+    aluno[nome].nota02 = n2;
+    aluno[nome].nota03 = n3;
+    cout << "Aluno cadastrado com sucesso!" << endl;
+}
+
+void excluirAluno(string nome){
+    aluno.erase(nome);
+}
+
+float calcularMedia(string nome){
+    float media = (aluno[nome].nota01 + aluno[nome].nota02 + aluno[nome].nota03)/3;
+    return media;
+}
+
+void listarAlunos(){
+    map<string, notas>::iterator it;
+
+    for(it = aluno.begin(); it != aluno.end(); it++){
+        cout << "Aluno: " << it->first << endl;
+        cout << "Nota 01 " << it->second.nota01 << endl;
+        cout << "Nota 02 " << it->second.nota02 << endl;
+        cout << "Nota 03 " << it->second.nota03 << endl;
+    }
+}
+
+void salvarArquivo(){
+    fstream arquivo;
+
+    arquivo.open("alunos.txt",ios::out|ios::app); //out é escrita e app é para não apagar os dados do arquivo escrito.
+    map<string, notas>::iterator it;
+
+    for(it = aluno.begin(); it != aluno.end(); it++){
+        arquivo << "Aluno: " << it-> first << endl;
+        arquivo << "Nota 01 " << it-> second.nota01 << endl;
+        arquivo << "Nota 02 " << it-> second.nota02 << endl;
+        arquivo << "Nota 03 " << it-> second.nota03 << endl;
+    }
+}
+
